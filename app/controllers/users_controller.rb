5f68fe26cc63513ec4c2757b4d72a993e71a6508
Komesu09 <67633415+Komesu09@users.noreply.github.com>
2020-07-16 11:59:33 +0900

    add.incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+  
   def edit
   end
 
@@ -10,13 +20,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-    @user = User.search(params[:keyword], current_user.id)
-
-    respond_to do |foemat|
-      format.html
-      format.json
-    end
   private
 
   def user_params
